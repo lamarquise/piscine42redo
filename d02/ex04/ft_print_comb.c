a5d897955823dfa07705cbc5680161fdb1b057b4
La Dauphine <e.lazo14@eabjm.org>
2018-03-23 00:27:11 +0100

    finished ex04, hope this goes through

--- d02/ex04/ft_print_comb.c
+++ d02/ex04/ft_print_comb.c
@@ -5,16 +5,44 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	print_func(int *tab)
+{
+	ft_putchar(tab[0] + 48);
+	ft_putchar(tab[1] + 48);
+	ft_putchar(tab[2] + 48);
+	if (tab[0] != 7)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+	else
+		ft_putchar('\n');
+}
+
 void	ft_print_comb(void)
 {
-	int		a;
-	int		b;
+	int		number[3];
 
-	a = 0;
-	b = 0;
-	while (a >= 7)
-	[
-		while 
+	number[0] = 0;
+	while (number[0] <= 7)
+	{
+		number[1] = number[0] + 1;
+		number[2] = number[0] + 2;
+		while (number[1] <= 8)
+		{
+			while (number[2] <= 9)		//(number[cursor] <= 9 - 2 + cursor)???
+			{
+				print_func(&number[0]);
+				number[2]++;
+			}
+			number[1]++;
+			number[2] = number[1] + 1;
+		}
+		number[0]++;
 	}
-	// use a 3 table i think
+}
+
+void	main(void)
+{
+	ft_print_comb();
 }
