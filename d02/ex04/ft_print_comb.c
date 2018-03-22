7b01382e765c679009fd4e6b3a1f8974b9f18983
La Marquise <e.lazo14@eabjm.org>
2018-03-22 18:51:34 +0100

    oops but yea adding an incomplete exercise

---
+++ d02/ex04/ft_print_comb.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int		a;
+	int		b;
+
+	a = 0;
+	b = 0;
+	while (a >= 7)
+	[
+		while 
+	}
+	// use a 3 table i think
+}
