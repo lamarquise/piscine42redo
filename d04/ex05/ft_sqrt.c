19cbde978a19b89b44e5f736599e9d8e828a8e3e
La Dauphine <e.lazo14@eabjm.org>
2018-04-12 01:56:02 +0200

    ok i ended up doing ex05

--- d04/ex05/ft_sqrt.c
+++ d04/ex05/ft_sqrt.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+	// Iterative Methode:
+
 int		ft_sqrt(int nb)		//could obviously be done iteratively by checking every number with a few clauses to speed up, but rather long winded
 {
-	if (nb > 0)
-		return 
-	return (0);				//work on this later, would be nice to do it interestingly
+	int		i;
+
+	i = 2;
+	if (nb == 1 || nb == 4)			// use == 4 here to get rid of = in <= of while
+		return (nb);
+	while (i < nb / 2)
+	{
+		if (i * i == nb)
+			return (i);
+		else
+			++i;
+	}
+	return (0);
 }
 
 int		main(int argc, char *argv[])
